511431e7e9c888c4efeb63f74b353ce737f372df
Holiday Ansar-Robins <holiday.ansarrobins@gmail.com>
2020-07-29 15:23:02 +0100

    Treasure lives read from game state

--- Shaun of the Dead/ListManagement/treasure_lives_list.py
+++ Shaun of the Dead/ListManagement/treasure_lives_list.py
@@ -2,11 +2,11 @@ import turtle
 
 class TreasureLivesList(object):
     """description of class"""
-    def __init__(self, gameStartManager, gameStateManager, level_key):
-        self.gameStartManager = gameStartManager
+    def __init__(self, gameStateManager):
+        self.gameStateManager = gameStateManager
         self.treasure_lives_list = []
 
-    def fill_treasure_lives_list(self, level_key):
-        self.treasure_lives_list = self.gameStartManager.get("Treasure_Lives", level_key)
+    def fill_treasure_lives_list(self):
+        self.treasure_lives_list = self.gameStateManager.get("Treasure_Lives")
 
 
