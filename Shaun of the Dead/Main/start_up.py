32078407497b676d6240618c0574071c2bcb68ad
Holiday Ansar-Robins <holiday.ansarrobins@gmail.com>
2020-08-16 16:14:50 +0100

    Changing the database used

--- Shaun of the Dead/Main/start_up.py
+++ Shaun of the Dead/Main/start_up.py
@@ -3,8 +3,8 @@ import DataManagement.game_state_manager as game_state_manager_
 import Main.shaun_of_the_dead as main_game_
 import DataManagement.initialise_new_game as initialise_new_game_
 
-#gameStartManager=game_start_manager_.GameStartManager("./ShaunOfTheDeadStartConditions.db")
 gameStartManager=game_start_manager_.GameStartManager("./ShaunOfTheDeadStartConditionsNPCandShaunexample WORKING BACKUP.db")
+#gameStartManager=game_start_manager_.GameStartManager("./ShaunOfTheDeadStartConditionsNPCandShaunexample.db")
 gameStateManager=game_state_manager_.GameStateManager("./ShaunOfTheDeadGameState.db")
 game=main_game_.ShaunOfTheDead(gameStateManager)
 print("Enter N for a new game")
